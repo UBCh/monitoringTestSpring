865444c4b57263b790c289722a45d6242ea80c9e
UBCh <u2b127@gmail.com>
2023-06-30 14:25:34 +0300

    no connection to database

---
+++ src/main/java/org/example/repository/ConnectionPool.java
@@ -0,0 +1,40 @@
+package org.example.repository;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class ConnectionPool {
+
+    private final HikariDataSource ds;
+
+
+    //      WITH SPRING PROPERTY SOURCE
+    public ConnectionPool(
+	    @Value("${spring.datasource.url}")
+	    String dbUrl,
+
+	    @Value("${spring.datasource.user}")
+	    String dbUser,
+
+	    @Value("${spring.datasource.password}")
+	    String dbPassword) {
+
+	HikariConfig config = new HikariConfig();
+
+	config.setJdbcUrl(dbUrl);
+	config.setUsername(dbUser);
+	config.setPassword(dbPassword);
+
+	ds = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+	return ds.getConnection();
+    }
+}
---
+++ src/main/java/org/example/repository/DBConnection.java
@@ -0,0 +1,16 @@
+package org.example.repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:root.properties")
+public class DBConnection {
+
+
+
+
+
+
+}
